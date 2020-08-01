859e9362552970c731f1a77c6c1e88365c23da46
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-08-01 06:26:40 -0700

    Updated html and js.

--- msa/msa.js
+++ msa/msa.js
@@ -25,8 +25,9 @@ const setCcuState = (currentCcu) => {
   const cameraDisplayDiv = state.camera.name;
   if (state.camera.name === currentCcu) {
     closeDisplayDiv(cameraDisplayDiv);
+    resetDisplayBtns();
     state.camera.name = "";
-    state.clicked = false;
+    state.camera.clicked = false;
   } else if (state.camera.name !== "") {
     closeDisplayDiv(cameraDisplayDiv);
     state.camera.name = currentCcu;
@@ -54,13 +55,51 @@ const openMonitorsDiv = (currentCcu) => {
 
 const selectDisplay = () => {
   const monitorBtnDivs = document.getElementsByClassName("monitor-btn-div");
-  console.log(monitorBtnDivs);
   for (let item of monitorBtnDivs) {
     item.addEventListener("click", () => {
       const selectedDisplay = event.currentTarget.dataset.display;
-      const displayBtn = document.getElementById(selectedDisplay);
-      displayBtn.className = "monitor-btn-div-active";
-      console.log(displayBtn);
+      setDisplayState(selectedDisplay);
     });
+  }
+};
+
+const setDisplayState = (selectedDisplay) => {
+  if (state.display.name === selectedDisplay) {
+    deselectDisplay(selectedDisplay);
+    state.display.name = "";
+    state.display.clicked = false;
+  } else if (state.display.name !== "") {
+    resetDisplayBtns();
+    setDisplayBtn(selectedDisplay);
+    state.display.name = selectedDisplay;
+    state.display.clicked = true;
+  } else {
+    state.display.name = selectedDisplay;
+    state.display.clicked = true;
+    setDisplayBtn(selectedDisplay);
+  }
+};
+
+const deselectDisplay = (selectedDisplay) => {
+  console.log(selectedDisplay, state.display);
+  const displayBtn = document.getElementById(selectedDisplay);
+  if (displayBtn !== null) {
+    displayBtn.className = "monitor-btn-div";
+  }
+};
+
+const setDisplayBtn = (selectedDisplay) => {
+  const displayBtn = document.getElementById(selectedDisplay);
+  if (displayBtn !== null) {
+    displayBtn.className = "monitor-btn-div-active";
+  }
+};
+
+const resetDisplayBtns = () => {
+  const displayBtns = document.getElementsByClassName("monitor-btn-div-active");
+  for (let item of displayBtns) {
+    if (item !== null) {
+      item.className = "monitor-btn-div";
+    }
   }
 };
